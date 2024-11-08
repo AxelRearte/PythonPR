8f41bb51b547dae4d16d784d446b35d0a49c47d6
axelrearte <axelrearte@hotmail.com>
2024-11-08 16:36:43 -0300

    Created parametrize declarator

--- tests/test_login_page_negative.py
+++ tests/test_login_page_negative.py
@@ -17,6 +17,35 @@ class TestNegativeScenarios:
 
     @pytest.mark.login
     @pytest.mark.negative
+    @pytest.mark.parametrize("username, password, expected_error_message", [("incorrectUser","Password123", "Your username is invalid!" ),
+                                                                              ("student", "Password1234", "Your password is invalid!" )])
+    def test_negative_login(self, driver, username, password, expected_error_message):
+        # Open page
+        # driver = webdriver.Chrome()
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+
+        # Type incorrect username student into Username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys(username)
+
+        # Type password Password123 into Password field
+        password_locator = driver.find_element(By.ID, "password")
+        password_locator.send_keys(password)
+
+        # Push Submit button
+        submit_button_locator = driver.find_element(By.XPATH, "//button[@id='submit']")
+        submit_button_locator.click()
+        time.sleep(2)
+
+        # Verify error message is displayed
+        error_message_locator = driver.find_element(By.ID, "error")
+        assert error_message_locator.is_displayed(), "Error message is not displayed, but it should"
+
+        # Verify error message is displayed for username
+        error_message = error_message_locator.text
+        assert error_message == expected_error_message, "Error message is not expected"
+
+
     def test_negative_username(self, driver):
         # Open page
         #driver = webdriver.Chrome()
@@ -43,20 +72,19 @@ class TestNegativeScenarios:
         error_message = error_message_locator.text
         assert error_message == "Your username is invalid!", "Error message is not expected"
 
-    @pytest.mark.login
-    @pytest.mark.negative
-    def test_negative_password(self, driver):
+
+    def test_negative_password(self, driver, username, password, expected_error_message):
         # Open page
         #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         driver.get("https://practicetestautomation.com/practice-test-login/")
 
         # Type username student into Username field
         username_locator = driver.find_element(By.ID, "username")
-        username_locator.send_keys("student")
+        username_locator.send_keys(username)
 
         # Type incorrect password Password123 into Password field
         password_locator = driver.find_element(By.ID, "password")
-        password_locator.send_keys("Password1234")
+        password_locator.send_keys(password)
 
         # Push Submit button
         submit_button_locator = driver.find_element(By.XPATH, "//button[@id='submit']")
@@ -69,4 +97,4 @@ class TestNegativeScenarios:
 
         # Verify error message is displayed for username
         error_message = error_message_locator.text
-        assert error_message == "Your password is invalid!", "Error message is not expected"
+        assert error_message == expected_error_message, "Error message is not expected"
