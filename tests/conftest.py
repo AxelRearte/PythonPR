1cd7e2968fecd26f7c2794eca281a9b28873f6ec
axelrearte <axelrearte@hotmail.com>
2024-11-08 17:17:05 -0300

    Parametrize Firefox

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,15 +5,26 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from urllib3 import request
 from webdriver_manager.firefox import GeckoDriverManager
 
 @pytest.fixture()
-def driver():
-    print("Creating chrome driver")
-    #my_driver = webdriver.Chrome()
-    my_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+def driver(request):
+    browser = request.config.getoption("--browser")
+    print(f"Creating {browser} driver")
+    if browser == "chrome":
+        my_driver = webdriver.Chrome()
+    elif browser == "firefox":
+        my_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    else:
+        raise TypeError(f"Expected 'chrome' or 'firefox', but got the {browser}")
     #my_driver = webdriver.Safari()
     yield my_driver
-    print("Close chrome driver")
+    print(f"Closing {browser} driver")
     my_driver.quit()
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="browser to execute tests(chrome or firefox)"
+    )
 
