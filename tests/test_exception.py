bbdd64fd918535bdbc9bd176f329185bcda06802
AxelRearte <axelrearte@hotmail.com>
2024-11-17 19:07:30 -0300

    Time out exception method

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -90,7 +90,7 @@ class TestExceptions:
 
     @pytest.mark.exceptions
     @pytest.mark.referenceexcepetion
-    def test_no_such_element_exception(self, driver):
+    def test_reference_exception(self, driver):
         # Open page
         driver.get("https://practicetestautomation.com/practice-test-exceptions/")
 
@@ -104,6 +104,23 @@ class TestExceptions:
         #Verify instructions is not displayed
         wait = WebDriverWait(driver, 10)
         assert wait.until(EC.invisibility_of_element_located((By.ID, "instructions")),"Instruction text element should not be displayed")
+
+    @pytest.mark.exceptions
+    @pytest.mark.timeout
+    def test_timeout_exception(self, driver):
+        # Open page
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/")
+
+        # Click Add button
+        add_button_locator = driver.find_element(By.ID, "add_btn")
+        add_button_locator.click()
+
+        # wait for 3 seconds to show the second row
+        wait = WebDriverWait(driver, 3)
+        row_2_input_element = wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@id='row2']/input")),"Failed waiting for Row 2 input to be visible")
+
+        # Verify Row 2 input field is displayed
+        assert row_2_input_element.is_displayed(), "Row 2 input should be displayed, but it's not"
 
 
 
