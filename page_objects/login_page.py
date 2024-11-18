162dcdf93c08de10c5810509bb701fbfdc0dc49f
AxelRearte <axelrearte@hotmail.com>
2024-11-18 20:26:11 -0300

    Page object Model Applied

--- page_objects/login_page.py
+++ page_objects/login_page.py
@@ -2,28 +2,29 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 
+from page_objects.base_page import BasePage
 from tests.config import USERNAME, click_element_by_xpath, PASSWORD, SUBMIT_BUTTON_XPATH
 from tests.conftest import driver
 from selenium.webdriver.support import expected_conditions as EC, wait
 
 
-class LoginPage:
+class LoginPage(BasePage):
     __url = "https://practicetestautomation.com/practice-test-login/"
     __username_field = (By.ID, "username")
     __password_field = (By.ID, "password")
     __submit_button = (By.XPATH, "//button[@id='submit']")
 
     def __init__(self, driver: WebDriver):
-        self._driver = driver
+        super().__init__(driver)
 
     def open(self):
-        self._driver.get(self.__url)
+        super().open_url(self.__url)
+
+
 
     def execute_login(self, username: str , password: str):
-        wait = WebDriverWait(self._driver,10)
-        wait.until(EC.visibility_of_element_located(self.__username_field))
-        self._driver.find_element(By.ID, "username").send_keys(USERNAME)
-        wait.until(EC.visibility_of_element_located(self.__password_field))
-        self._driver.find_element(By.ID, "password").send_keys(PASSWORD)
-        wait.until(EC.visibility_of_element_located(self.__submit_button))
-        click_element_by_xpath(self._driver, SUBMIT_BUTTON_XPATH)
+        super()._type(self.__username_field, username)
+        super()._type(self.__password_field, password)
+        super()._click(self.__submit_button)
+
+
